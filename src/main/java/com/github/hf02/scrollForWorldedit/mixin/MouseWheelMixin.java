84c6e445bc1e3d5ff56b28fad0ebee4e4aeb1c3a
hf02 <55464333+hf02@users.noreply.github.com>
2022-07-12 21:11:40 -0400

    the mixin

--- src/main/java/com/github/hf02/scrollForWorldedit/mixin/MouseWheelMixin.java
+++ src/main/java/com/github/hf02/scrollForWorldedit/mixin/MouseWheelMixin.java
@@ -1,6 +1,6 @@
 package com.github.hf02.scrollForWorldEdit.mixin;
 
-import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.Mouse;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -8,10 +8,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.github.hf02.scrollForWorldEdit.ScrollForWorldEdit;
 
-@Mixin(TitleScreen.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
+@Mixin(Mouse.class)
+public class MouseWheelMixin {
+	@Inject(at = @At("HEAD"), method = "onMouseScroll()", cancellable = true)
+	private void onMouseScroll(CallbackInfo info) {
 		ScrollForWorldEdit.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
