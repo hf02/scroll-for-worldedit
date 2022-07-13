0f04be9a7ae7e49931e918230a2ec3140d2d5e6b
hf02 <55464333+hf02@users.noreply.github.com>
2022-07-12 20:00:12 -0400

    first commit

---
+++ src/main/java/com/github/hf02/scrollForWorldedit/mixin/ExampleMixin.java
@@ -0,0 +1,17 @@
+package com.github.hf02.scrollForWorldedit.mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.github.hf02.scrollForWorldedit.ScrollForWorldedit;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		ScrollForWorldedit.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
