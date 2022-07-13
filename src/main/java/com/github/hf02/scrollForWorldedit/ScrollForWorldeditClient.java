0e5dc17bbf22b43e3fdb9ded44797e39ca42fbba
hf02 <55464333+hf02@users.noreply.github.com>
2022-07-12 20:30:11 -0400

    client test

--- src/main/java/com/github/hf02/scrollForWorldedit/ScrollForWorldeditClient.java
+++ src/main/java/com/github/hf02/scrollForWorldedit/ScrollForWorldeditClient.java
@@ -5,7 +5,6 @@ import org.lwjgl.glfw.GLFW;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 
@@ -18,6 +17,7 @@ public class ScrollForWorldeditClient implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
+		ScrollForWorldedit.LOGGER.info("active...");
 		keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
 				"key.scroll_for_worldedit.spook",
 				InputUtil.Type.KEYSYM,
@@ -26,7 +26,7 @@ public class ScrollForWorldeditClient implements ClientModInitializer {
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			while (keyBinding.wasPressed()) {
-				MinecraftClient.getInstance().player.sendChatMessage("//move 1 -s");
+				client.player.sendChatMessage("//move 1 -s");
 			}
 		});
 	}
