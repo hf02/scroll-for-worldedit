e7191ed58c84bbae06e77d45382114d0325d0aaf
Daniel Orr <orrdaniel73@gmail.com>
2024-07-15 15:20:07 +0100

    Update to 1.21

--- src/main/java/com/github/hf02/scrollForWorldEdit/mixin/TextHudMixin.java
+++ src/main/java/com/github/hf02/scrollForWorldEdit/mixin/TextHudMixin.java
@@ -3,6 +3,7 @@ package com.github.hf02.scrollForWorldEdit.mixin;
 import com.github.hf02.scrollForWorldEdit.client.ScrollForWorldEditClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.render.RenderTickCounter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,11 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class TextHudMixin {
 
 	@Inject(at = @At("RETURN"), method = "render")
-	public void render(
-		DrawContext context,
-		float tickDelta,
-		CallbackInfo info
-	) {
+	public void render(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
 		ScrollForWorldEditClient.textRenderer.renderText(context);
 	}
 }
